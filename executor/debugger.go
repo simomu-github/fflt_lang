9af8ac14ef30bf0bf3bb7506b7770f518e56108a
simomu-github <simomutter@gmail.com>
2024-10-09 21:35:37 +0900

    Fix table order for debugger

--- executor/debugger.go
+++ executor/debugger.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -273,10 +274,15 @@ func (d *Debugger) showStack() {
 
 func (d *Debugger) showHeap() {
 	d.heapTableWriter.ClearRows()
-	for k, v := range d.executor.heap {
+	keys := []int{}
+	for k := range d.executor.heap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
 		d.heapTableWriter.Append([]string{
-			fmt.Sprintf("%d", k),
-			fmt.Sprintf("%d", v),
+			fmt.Sprintf("%d", key),
+			fmt.Sprintf("%d", d.executor.heap[key]),
 		})
 	}
 
@@ -287,10 +293,15 @@ func (d *Debugger) showHeap() {
 
 func (d *Debugger) showLabelMap() {
 	d.labelMapTableWriter.ClearRows()
-	for k, v := range d.executor.LabelMap {
+	keys := []string{}
+	for k := range d.executor.LabelMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
 		d.labelMapTableWriter.Append([]string{
-			k,
-			fmt.Sprintf("%d", v),
+			key,
+			fmt.Sprintf("%d", d.executor.LabelMap[key]),
 		})
 	}
 	fmt.Printf("\n")
